64ebfbdefd8458970d5a7458bb5f3e498bc16a97
catnuko <346746061@qq.com>
2023-10-20 21:21:05 +0800

    feat: mod

--- houtu-renderer/src/image/mod.rs
+++ houtu-renderer/src/image/mod.rs
@@ -1,54 +1,36 @@
 use bevy::{
+    asset::AssetLoader,
     prelude::*,
     tasks::{AsyncComputeTaskPool, Task},
     utils::HashMap,
 };
-
-use self::fetch_img_job::FetchImgJob;
-
-mod fetch_img_job;
-pub struct Plugin;
-
-impl bevy::prelude::Plugin for Plugin {
-    fn build(&self, app: &mut bevy::prelude::App) {}
+#[derive(Asset, TypePath, Debug, Deserialize)]
+pub struct NoExtensionAsset {}
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum NoExtensionAssetLoaderError {
+    /// An [IO](std::io) Error
+    #[error("Could load shader: {0}")]
+    Io(#[from] std::io::Error),
+    /// A [RON](ron) Error
+    #[error("Could not parse RON: {0}")]
+    RonSpannedError(#[from] ron::error::SpannedError),
 }
-#[derive(Resource)]
-pub struct ImgServer {
-    img_storage: HashMap<u32, String>,
-    next_id: u32,
-}
-impl ImgServer {
-    fn get_id(&mut self) -> u32 {
-        let id = self.next_id;
-        self.next_id += 1;
-        return id;
-    }
-    pub fn get(&mut self, src: String) {
-        let id = self.get_id();
-    }
-    pub fn remove_img(&mut self, id: &u32) {
-        self.img_storage.remove(id);
-    }
-}
+#[derive(Default)]
+pub struct NoExtensionAssetLoader;
 
-#[derive(Event)]
-pub struct FetchImg {
-    pub src: String,
-}
-fn task_update(
-    events: EventReader<FetchImg>,
-    mut job_spawner: houtu_jobs::JobSpawner,
-    asset_server: ResMut<AssetServer>,
-    mut finished_jobs: bevy_jobs::FinishedJobs,
-) {
-    for fetch_img in events.iter() {
-        if fetch_img.src.starts_with("http") || fetch_img.src.starts_with("https") {
-            job_spawner.spawn(FetchImgJob {
-                url: fetch_img.src.clone(),
-            })
-        } else {
-            let img = asset_server.load(fetch_img.src);
-        }
+impl AssetLoader for NoExtensionAssetLoader {
+    type Assets = NoExtensionAsset;
+    type Settings = ();
+    type Error = NoExtensionAssetLoaderError;
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut bevy::asset::LoadContext,
+    ) -> bevy::utils::BoxedFuture<'a, Result<(), bevy::asset::Error>> {
+        
     }
-    while let Some(result) = finished_jobs.take_next::<FetchImgJob>() {}
+    fn extensions(&self) -> &[&str] {
+        &["noextension"]
+    }
 }
